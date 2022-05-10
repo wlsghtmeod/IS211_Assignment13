07fabed02726a31acd5c5adf340ebe9057bee0c6
jaino <wlsghtmeod@gmail.com>
2022-05-10 14:02:10 +0900

    Assignment

--- recursion.py
+++ recursion.py
@@ -1,13 +1,30 @@
-def fibornnaci():
-    pass
+def fibonacci(n, sec_last, last):
+    if n-1 == 0:
+        return sec_last
+    
+    else:
+        new_last = second_last + last
+        second_last = last
+        return fibonacci(n-1,second_last,new_last)
 
 def gcd(a,b):
-    pass
+    if a == 0:
+        return b
+    
+    return gcd(b%a, a)
+
 
 def compareTo(s1,s2):
-    pass
+    if a < b:
+        return -1
+    elif a > b:
+        return -1
+    else:
+        return 0
 
 if __name__ == "__main__":
-    print(fibornnaci)
-    print(gcd)
-    print(compareTo)
+    print(fibonacci(10,0,1))
+    a=int(input())
+    b=int(input())
+    print(gcd(a,b))
+    print(compareTo(a,b))
